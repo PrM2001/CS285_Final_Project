229b5226fbc17fa7aae8464a2476934148688d17
jjaison <joeljaison13@berkeley.edu>
2023-12-08 19:02:24 -0800

    added agent

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='rhddp',
+    version='0.1.0',
+    packages=['rhddp'],
+)
+
+#copied from cs285 homework, trying to make this a package for easier imports
